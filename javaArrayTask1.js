9d160f23b1630e82d447b215b14689d0bdc71284
SudhaGurung-cedcoss <sudhakumarigurung@cedcoss.com>
2022-08-02 18:24:08 +0530

    Java array Task5

--- javaArrayTask1.js
+++ javaArrayTask1.js
@@ -1,25 +1,20 @@
-var arr=[{'Company':'samsung','Model':'Galaxy','Memory':'64 ','Price':'15000'},
-{'Company':'Nokia','Model':'S370','Memory':'128 ','Price':'22000'},
-{'Company':'Apple','Model':'S12','Memory':'64 ','Price':'25000'},
-{'Company':'Xiaomi','Model':'Note','Memory':'32 ','Price':'12000'},
-{'Company':'Motarola','Model':'G10','Memory':'32 ','Price':'15000'}];
+var arr=[{'Company':'samsung','Model':'Galaxy','Memory':'64 ','Price':15000,'Quantity':0},
+{'Company':'Nokia','Model':'S370','Memory':'128 ','Price':22000,'Quantity':0},
+{'Company':'Apple','Model':'S12','Memory':'64 ','Price':25000,'Quantity':0},
+{'Company':'Xiaomi','Model':'Note','Memory':'32 ','Price':12000,'Quantity':0},
+{'Company':'Motarola','Model':'G10','Memory':'32 ','Price':15000,'Quantity':0}];
 
 printer(arr);
 function printer(arr){
-
-
-    
     var text="";
     for (var i=0;i<arr.length;i++) {
         text+='<tr>';
-        text+='<td>'+arr[i].Company+'</td>'+'<td>'+arr[i].Model+'</td>'+'<td>'+arr[i].Memory+'</td>'+'<td>'+arr[i].Price+'</td>';
-    }
+        text+='<td>'+arr[i].Company+'</td>'+'<td>'+arr[i].Model+'</td>'+'<td>'+arr[i].Memory+'</td>'+'<td>'+arr[i].Price+'</td>'+'<td>'+arr[i].Quantity+'</td>';
+     }
     //text+='<td>'+samsung[0].Company+'</td>'+'<td>'+samsung[0].Model+'</td>'+'<td>'+samsung[0]["Memory(GB)"]+'</td>'+'<td>'+samsung[0].Price+'</td>';
-    
-    text+='</tr>';
+     text+='</tr>';
     document.getElementById('tbody').innerHTML=text;
-
-}
+   }
 
 function addData(){
     alert("hello");
@@ -39,12 +34,121 @@ function addData(){
     alert(arr);
     printer(arr);
  }
+
+
+    var sumProduct;
+    var productArray=[];
+    var totalAmount=0;
+    var totalQuantity=0;
+    function add(){
+
+    var selectProduct=document.getElementById('selectProduct').value;
+    quantity=document.getElementById('quantity').value;
+    var value=searchIndex(selectProduct);
+    var c=arr[value].Company;
+    var p=arr[value].Price;
+    var p1=searchItemExist(selectProduct);
+    alert(p1);
+    if(p1==-1)
+    {
+    sumProduct={Company:c,Quantity:quantity,Amount:p};
+    productArray.push(sumProduct);
+
+    console.log(productArray);
+     }
+    else{
+        productArray[p1].Quantity=parseInt(productArray[p1].Quantity)+parseInt(quantity);
+
+        productArray[p1].Amount=parseInt(productArray[p1].Amount)*parseInt(quantity);
+    }
+
+    showData();
+  }
+
+
+  function searchItemExist(selectProduct){
+    for(let i=0;i<productArray.length;i++)
+    {
+       // alert(selectProduct);
+        if(productArray[i].Company==selectProduct)
+        {
+          alert(selectProduct);
+          
+
+           return i;
+        }
+       
+       
+    }
+    return -1;
+  }
+
+  function showData()
+  {
+    console.log("pa:",productArray);
+   
+    var textt="";
+    for(i=0;i<productArray.length;i++)
+    {
+
+        
+           // console.log("hello",productArray[i])
+            textt+='<tr>'+'<td>'+productArray[i].Company+'</td>'+'<td>'+productArray[i].Quantity+'</td>'+'<td>'+productArray[i].Amount+'</td>'+'</tr>';
+            console.log("textt="+textt);
+           
+        
+    }
+    document.getElementById('table4').style.display="block";
+    document.getElementById('bill').innerHTML=textt ;
+    
+  }
+
+
   
+  function searchIndex(selectOption){
+    for(i=0;i<arr.length;i++)
+    {
+        if(arr[i].Company==selectOption)
+        return i;
+       
+    }
+  }
+
+
+  function billGen(){
+   var billgenerate="";
+    for(i=0;i<productArray.length;i++)
+    {
+         totalQuantity+=parseInt(productArray[i].Quantity);
+         totalAmount+=parseInt(productArray[i].Amount);
+        billgenerate='<tr>'+'<td>'+"total"+'</td>'+'<td>'+totalQuantity+'</td>'+'<td>'+totalAmount+'</td>'+'</tr>';
+            
+    }
+    document.getElementById('bill').innerHTML+=billgenerate;
+}
+
+ 
+
+    
+
+
+     
+      
+
+ 
+
+
+
+  
+
+  
+
+
+    
  
    function sorting(){
   
     var sort=document.getElementById('sort').value;
-    
     var sort_by=document.getElementById('sort_by').value;
     //   alert(sort_by);
       if(sort=="-1")
@@ -143,7 +247,7 @@ function addData(){
     if(item=='samsung'){
    var text2='<tr>';
     
-     text2+='<td>'+arr[0].Company+'</td>'+'<td>'+arr[0].Model+'</td>'+'<td>'+arr[0].Memory+'</td>'+'<td>'+arr[0].Price+'</td>';
+    text2+='<td>'+arr[0].Company+'</td>'+'<td>'+arr[0].Model+'</td>'+'<td>'+arr[0].Memory+'</td>'+'<td>'+arr[0].Price+'</td>';
     text2+='</tr>';
     
     document.getElementById('thead2').style.display="block";
@@ -348,7 +452,4 @@ if(item=='Nokia'){
  
      
  
-
-
-
 
